caf23e71987469f167d460d68ba4856fcc9eb70a
Russell Ryan <rryan@duckdunn.local>
2019-10-01 21:22:42 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,22 +1,42 @@
 
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
+import numpy
+from setuptools import Extension,setup
+from glob import glob
+import os
 
 
-setup(name='polyclip',\
-      version='1.0', \
-      author='Russell Ryan', \
-      author_email='rryan@stsci.edu',\
+AUTHOR='Russell Ryan'
+PKG='polyclip'
+info={
+    '__author__': AUTHOR,
+    '__version__': '1.0',
+    '__maintainer__': AUTHOR,
+    '__email__': 'rryan@stsci.edu',
+    '__credits__': ['python: {}'.format(AUTHOR),'C: JD Smith'],
+    '__doc__':'Python driver for polyclip routines written by JD Smith',
+
+}
+
+# Generate package metadata
+with open(os.path.join(PKG, 'info.py'), 'w+') as fp:
+    for k, v in info.items():
+        fp.write('{} = "{}"{}'.format(k, v, os.linesep))
+
+
+setup(name=PKG,
+      version=info['__version__'],
+      author=info['__author__'],
+      author_email=info['__email__'],
       keywords="Sutherland Hodgman polygon clipping C python",\
-      description='Python API for calling JD Smith polyclip',\
+      description=info['__doc__'],
       license='MIT',\
       packages=['polyclip'],\
-      #install_requires=['numpy'],\
+      install_requires=['numpy'],\
       classifiers=["Development Status :: 5 Production/Stable ",
                    'Intended Audience :: Science/Research',
                    'Topic :: Scientific/Engineering :: Astronomy',],\
-      ext_modules=[Extension('cpolyclip',['polyclip/_polyclip.c',\
-                                          'polyclip/polyclip.c'])],\
-      include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
-
-
+      ext_modules=[
+          Extension('polyclip.cpolyclip',glob(os.path.join(PKG,'src','*.c')),
+                    include_dirs=[os.path.join(PKG,'include'),
+                                  numpy.get_include()])])
+      
